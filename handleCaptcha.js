309072535512a3115ad8c8aa54c6ac708120dce0
SKolevatov <sergei.kolevatov@nytimes.com>
2018-08-24 12:19:30 -0400

    ES6

--- handleCaptcha.js
+++ handleCaptcha.js
@@ -1,14 +1,22 @@
-var form = document.getElementById('form');
-var token;
-var sendToGoogleButton = document.getElementById("button-send");
+const form = document.getElementById('form');
+let token;
+const sendToGoogleButton = document.getElementById("button-send");
+const tokenJumbotron = document.getElementById("token");
+const tokenContainer = document.getElementById("token-place");
+const googleRespJumbotron = document.getElementById("google");
+const googleRespContainer = document.getElementById("response");
+
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    document.getElementById("token").classList.remove('d-none');
+    tokenJumbotron.classList.remove('d-none');
     token = grecaptcha.getResponse();
-    document.getElementById("token-place").innerHTML = token;
+    tokenContainer.innerHTML = token;
     sendToGoogleButton.classList.remove('d-none');
 });
-sendToGoogleButton.addEventListener('click', function (event) {
+
+
+
+sendToGoogleButton.addEventListener('click', (event) => {
     event.preventDefault();
     fetch('https://www.google.com/recaptcha/api/siteverify', {
         method: 'post',
@@ -16,10 +24,8 @@ sendToGoogleButton.addEventListener('click', function (event) {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-    }).then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        document.getElementById("google").classList.remove('d-none');
-        document.getElementById("response").innerHTML = JSON.stringify(data);
+    }).then((response) => response.json()).then(function(data) {
+        googleRespJumbotron.classList.remove('d-none');
+        googleRespContainer.innerHTML = JSON.stringify(data);
     });
 });
